b9ca3bdddbd31f464a7d37b49c93298b6c11dad6
Abe Jellinek <abe@newscenter.com>
2015-11-20 20:19:39 -0800

    First working web service version! Plain text response right now, but functionality comes first.

--- od-web/src/main/java/me/abje/xmptest/web/WebController.java
+++ od-web/src/main/java/me/abje/xmptest/web/WebController.java
@@ -1,7 +1,21 @@
 package me.abje.xmptest.web;
 
+import com.adobe.xmp.XMPException;
+import com.adobe.xmp.XMPMeta;
+import com.adobe.xmp.XMPMetaFactory;
+import me.abje.xmptest.*;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDDocumentCatalog;
+import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class WebController {
@@ -9,4 +23,40 @@ public class WebController {
     public String index() {
         return "index";
     }
+
+    @RequestMapping(value = "/upload", method = RequestMethod.POST)
+    @ResponseBody
+    public String upload(@RequestParam("file") MultipartFile file) throws IOException, XMPException {
+        InputStream in = file.getInputStream();
+
+        PDDocument doc = PDDocument.load(in);
+        List<Table> tables = new ArrayList<>();
+
+        tables.addAll(read(new AnnotationDataStorage(), doc));
+        tables.addAll(read(new AttachmentDataStorage(), doc));
+        tables.addAll(read(new FormDataStorage(), doc));
+        tables.addAll(read(new XMPDataStorage(), doc));
+
+        doc.close();
+
+        return tables.toString();
+    }
+
+    private List<Table> read(DataStorage storage, PDDocument doc) throws IOException, XMPException {
+        PDDocumentCatalog catalog = doc.getDocumentCatalog();
+        XMPMeta xmp;
+        if (catalog.getMetadata() == null) {
+            xmp = XMPMetaFactory.create();
+        } else {
+            xmp = XMPMetaFactory.parse(catalog.getMetadata().createInputStream());
+        }
+
+        return storage.read(doc, xmp);
+    }
+
+    @ModelAttribute("_csrf")
+    public CsrfToken csrf(HttpServletRequest request) {
+        return (CsrfToken) request.getAttribute("_csrf");
+    }
+
 }
