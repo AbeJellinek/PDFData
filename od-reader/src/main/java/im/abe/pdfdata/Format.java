976b9452ab01c7c7b83ce20e51602ff07d5001fe
Abe Jellinek <abe@newscenter.com>
2017-03-27 17:13:32 -0700

    Documented Format enum.

--- od-reader/src/main/java/im/abe/pdfdata/Format.java
+++ od-reader/src/main/java/im/abe/pdfdata/Format.java
@@ -1,12 +1,34 @@
 package im.abe.pdfdata;
 
 public enum Format {
+    /**
+     * CSV data format. Columns separated by commas, rows by lines. Values can be quoted.
+     */
     CSV(".csv", "text/csv"),
+
+    /**
+     * JSON format. Written using JavaScript object syntax.
+     */
     JSON(".json", "application/json"),
+
+    /**
+     * XML-based representation of raw RDF data.
+     */
     RDF_XML(".xml", "application/rdf+xml"),
+
+    /**
+     * The most concise, idiomatic RDF format.
+     */
     TURTLE(".turtle", "text/turtle");
 
+    /**
+     * The file extension associated with this format.
+     */
     private String extension;
+
+    /**
+     * This format's MIME type.
+     */
     private String mime;
 
     Format(String extension, String mime) {
@@ -22,6 +44,12 @@ public enum Format {
         return mime;
     }
 
+    /**
+     * Find a format by the given name. "RDF", "RDF_XML", "RDF/XML", "RDFXML" all map to the same thing.
+     *
+     * @param name The name to look up.
+     * @return The format if found; CSV otherwise.
+     */
     public static Format find(String name) {
         switch (name.toUpperCase()) {
             case "JSON":
